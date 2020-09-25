dacb1bf23aa7d1fa861e6d5834832ca397158ed5
f-santos <frederic.santos@u-bordeaux.fr>
2020-09-25 16:23:12 +0200

    Initial commit

---
+++ ob-julia.el
@@ -0,0 +1,21 @@
+;;; ob-julia --- Org babel support for Julia language
+
+;; Copyright (C) 2020 Frédéric Santos
+
+;; Author: Frédéric Santos
+;; Version: 0.0
+;; Keywords: babel, julia, literate programming, org
+;; URL: https://gitlab.com/f-santos/ob-julia
+
+;; This file is *not* part of GNU Emacs.
+
+;;; Commentary:
+;; This package intends to add an elementary support for Julia language
+;; in Org mode.  It is still at a very early stage of development.
+
+;;; Code:
+(require 'cl-lib)
+(require 'ob)
+
+(provide 'ob-julia)
+;;; ob-julia.el ends here
