49af4dfec24740a5f0d3eb240adcf9edbd88f719
f-santos <frederic.santos@u-bordeaux.fr>
2020-10-07 13:07:59 +0200

    Julia dependencies are now installed if needed

--- ob-julia-startup.jl
+++ ob-julia-startup.jl
@@ -1,3 +1,10 @@
+# Check that required packages are installed:
+if Base.find_package("CSV") == nothing | Base.find_package("DelimitedFiles") == nothing
+    using Pkg
+    Pkg.add("CSV")
+    Pkg.add("DelimitedFiles")
+end
+
+# Load required packages:
 using CSV
-using DataFrames
 using DelimitedFiles
