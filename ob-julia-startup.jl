7b45b5fad2bf039b224221d3dcc7da593b7e7077
f-santos <frederic.santos@u-bordeaux.fr>
2020-10-07 13:15:07 +0200

    Typo

--- ob-julia-startup.jl
+++ ob-julia-startup.jl
@@ -1,5 +1,5 @@
 # Check that required packages are installed:
-if Base.find_package("CSV") == nothing | Base.find_package("DelimitedFiles") == nothing
+if Base.find_package("CSV") == nothing || Base.find_package("DelimitedFiles") == nothing
     using Pkg
     Pkg.add("CSV")
     Pkg.add("DelimitedFiles")
