74ef9d6863160f98a4aadf36159c96d71b3960b5
f-santos <frederic.santos@u-bordeaux.fr>
2020-10-08 16:49:54 +0200

    Add Julia function to write objects

--- ob-julia-startup.jl
+++ ob-julia-startup.jl
@@ -8,3 +8,14 @@ end
 # Load required packages:
 using CSV
 using DelimitedFiles
+
+# Perso function to write Julia objects into CSV files:
+function ob_julia_write(bodycode, filename, has_header)
+    try
+        CSV.write(filename, bodycode, delim = "\t", writeheader = has_header)
+    catch err
+        if isa(err, ArgumentError) | isa(err, MethodError)
+            writedlm(filename, bodycode)
+        end
+    end
+end
