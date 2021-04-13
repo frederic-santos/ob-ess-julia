5632895aaa3b34d3cbca530266c8e32b27c3590d
f-santos <frederic.santos@u-bordeaux.fr>
2021-04-13 18:37:29 +0200

    Suppressor.jl is not needed anymore

--- ob-ess-julia-startup.jl
+++ ob-ess-julia-startup.jl
@@ -4,13 +4,11 @@
 csvp = Base.find_package("CSV") == nothing
 delimp = Base.find_package("DelimitedFiles") == nothing
 pipep = Base.find_package("Pipe") == nothing
-suppp = Base.find_package("Suppressor") == nothing
 if any([csvp, delimp, pipep, suppp])
     using Pkg
     Pkg.add("CSV")
     Pkg.add("DelimitedFiles")
     Pkg.add("Pipe")
-    Pkg.add("Suppressor")
 end
 
 # Load required packages:
